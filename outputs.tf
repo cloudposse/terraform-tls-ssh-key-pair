3d9e554ec4a5a3791b8ea0c4e7c3e2fe25094d8c
Erik Kristensen <erik@erikkristensen.com>
2021-05-18 08:18:53 -0600

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -7,3 +7,8 @@ output "public_key" {
   value       = join("", tls_private_key.default.*.public_key_openssh)
   description = "Content of the generated public key"
 }
+
+output "private_key" {
+  value       = tls_private_key.default.private_key_pem
+  description = "Content of the generated private key"
+}
