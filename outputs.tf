2ffd88093f665a1fb6bb493aa38766547e46c6b9
Erik Kristensen <erik@erikkristensen.com>
2021-05-21 12:51:41 -0600

    Add private key as an output (#31)

--- outputs.tf
+++ outputs.tf
@@ -7,3 +7,9 @@ output "public_key" {
   value       = join("", tls_private_key.default.*.public_key_openssh)
   description = "Content of the generated public key"
 }
+
+output "private_key" {
+  value       = var.private_key_output_enabled ? tls_private_key.default.private_key_pem : null
+  description = "Content of the generated private key"
+  sensitive   = true
+}
